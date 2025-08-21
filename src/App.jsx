c597dcd88c19f2738242f86611ab851a7bf7002f
Nisar <emanatehome.social@gmail.com>
2025-08-21 10:52:09 +0530

    Initial commit - Emanate Home Web App

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Emanate Home</h1>
+      <p>Welcome to your Progressive Web App 🚀</p>
+    </div>
+  );
+}
+
+export default App;
